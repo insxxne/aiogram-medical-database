1f33a0378d30c72ba865d6d404c49e3dd84ceaa1
end <zag4545@gmail.com>
2023-09-14 12:27:15 +0500

    added main logic of parse site

--- parse_part.py
+++ parse_part.py
@@ -1,3 +1,4 @@
+import json
 import pprint
 
 import requests
@@ -6,16 +7,26 @@ from bs4 import BeautifulSoup
 url = 'http://www.smclinic.ru/diseases/'
 
 headers = {
-    'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
 }
+
+
+def set_default(obj):
+    if isinstance(obj, set):
+        return list(obj)
+    raise TypeError
+
 
 def getMiniDesc(link):
     page_url = 'http://www.smclinic.ru' + link
     if requests.get(page_url).status_code == 200:
         page_r = requests.get(page_url)
         page_soup = BeautifulSoup(page_r.content, 'html.parser')
-        description = page_soup.select_one('div[itemprop=description]').text
-        return description
+        if page_soup.select_one('div[itemprop=description]') != None:
+            description = page_soup.select_one('div[itemprop=description]').text
+            return description
+        else:
+            return getDesc(link)
 
 
 def getDesc(link):
@@ -23,7 +34,7 @@ def getDesc(link):
     if requests.get(page_url).status_code == 200:
         page_r = requests.get(page_url)
         page_soup = BeautifulSoup(page_r.content, 'html.parser')
-        main_text = page_soup.find('div', attrs={'class' : 'b-text-block-6__text text'}).text
+        main_text = page_soup.find('div', attrs={'class': 'b-text-block-6__text text'}).text
         return main_text
 
 
@@ -31,30 +42,24 @@ if requests.get(url).status_code == 200:
     r = requests.get(url, headers=headers)
     soup = BeautifulSoup(r.content, 'html.parser')
 
-    diseases = soup.find('div', attrs={'class' : 'diseases-list__items'})
+    diseases = soup.find('div', attrs={'class': 'diseases-list__items'})
 
-    item_element = {}
-    count = 1
+    #
+    # for item in diseases.find_all('a'):
+    #     if 'www.sm-stomatology.ru/patients/diseases/' in item.get('href'):
+    #         continue
+    #     else:
+    #         item_url = url + item.get('href')
+    #         item_name = item.text
+    #         item_desc = getMiniDesc(item.get('href'))
+
+        # with open('db_dict.json', 'w', encoding='utf-8') as file:
+        #     json.dump(item_element, file, indent=4, ensure_ascii=False, default=set_default)
 
 
+    with open("db_dict.json", 'r', encoding='utf-8') as file:
+        json_data = file.read()
+        data = json.loads(json_data)
 
-    for item in diseases.find_all('a'):
-        item_url = url + item.get('href')
-        item_name = item.text
-        item_desc = getMiniDesc(item.get('href'))
+        print(len(data))
 
-
-        item_element[count] = {
-                'Имя' : {
-                    item_name
-                },
-                'Ссылка' : {
-                    item_url
-                },
-                'Описание' : {
-                    item_desc
-                }
-            }
-
-        print(count)
-    pprint.pprint(item_element[1])
